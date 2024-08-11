0dd6466f3b57872d3d9192b9ecd18f35af97a6e1
Johannes Bjärehall <jojeskate@hotmail.com>
2024-08-11 21:27:30 +0200

    day 7 and 8

--- Days/Day07.cs
+++ Days/Day07.cs
@@ -1,26 +1,114 @@
 ﻿using AdventOfCode2023.Attributes;
 using AdventOfCode2023.Intefaces;
 using AdventOfCode2023.Services;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
 
 namespace AdventOfCode2023.Days;
 
-[DayInfo("Wait For It", "Day 7")]
+[DayInfo("Camel Cards", "Day 7")]
 internal class Day07 : ISolution
 {
     private readonly string[] _input;
+    private readonly List<(int[] hand, int bid)> hands;
 
-    public Day07(LoadInputService inputService) 
+    private readonly Dictionary<string, int> groupRanks = new()
+    {
+        {
+            "5", 5
+        },
+        {
+            "41", 4
+        },
+        {
+            "32", 3
+        },
+        {
+            "31", 3
+        },
+        {
+            "22", 2
+        },
+        {
+            "21", 1
+        },
+        {
+            "1", 0
+        }
+    };
+
+    public Day07(LoadInputService inputService)
     {
         _input = inputService.GetInputAsLines(nameof(Day07));
+        hands = _input.Select(line => ParseHand(line.Split(" ", StringSplitOptions.RemoveEmptyEntries))).ToList();
+
     }
+
+    private (int[] hand, int bid) ParseHand(string[] inputLine)
+        => (inputLine[0].Select(ConvertCard).ToArray(), int.Parse(inputLine[1]));
+
+    private int ConvertCard(char card)
+    => card switch
+    {
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 11,
+        'T' => 10,
+        _ => int.Parse(card.ToString())
+    };
 
     public void SolvePartOne()
     {
-        Console.WriteLine("");
+        var handRanks = hands.Select(hand =>
+        {
+            var cardGroups = hand.hand
+            .GroupBy(card => card)
+            .GroupBy(g => g.Count()).Select(x => x.Key)
+            .OrderByDescending(x => x)
+            .Aggregate("", (a, b) => a + b);
+
+            int rank = 0;
+            groupRanks.TryGetValue(cardGroups, out rank);
+
+            return (rank, hand);
+        })
+        .GroupBy(hand => hand.rank)
+        .ToDictionary(hand => hand.Key, hand => hand.Select(x => x.hand).ToList());
+
+        foreach (var item in handRanks)
+        {
+            item.Value.Sort(handComparer);
+        }
+
+        int counter = 1;
+        int result = handRanks.Select(x => x.Key).OrderBy(x => x).Select(rank => handRanks[rank].Select(x => x.bid * counter++).Sum()).Sum();
+
+        Console.WriteLine(result);
+    }
+
+    Comparison<(int[] hand, int bid)> handComparer = (a, b) =>
+    {
+        for (int i = 0; i < a.hand.Length; i++)
+        {
+            if (a.hand[i] > b.hand[i])
+                return 1;
+            if (a.hand[i] < b.hand[i])
+                return -1;
+        }
+
+        return 0;
+    };
+
+    private int OrderHand(int[] A, int[] B)
+    {
+        return 0;
     }
 
     public void SolvePartTwo()
     {
         Console.WriteLine("");
     }
+
+
 }
---
+++ Days/Day08.cs
@@ -0,0 +1,128 @@
+﻿using AdventOfCode2023.Attributes;
+using AdventOfCode2023.Intefaces;
+using AdventOfCode2023.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AdventOfCode2023.Days;
+
+[DayInfo("Haunted Wasteland", "Day 8")]
+public class Day08 : ISolution
+{
+    private readonly string[] _input;
+    private readonly string _instructions;
+    private List<Node> _nodeList = new();
+
+    public Day08(LoadInputService inputService)
+    {
+        _input = inputService.GetInputAsLines(nameof(Day08));
+        _instructions = _input[0];
+
+
+
+        foreach (var textNode in _input.Skip(2))
+        {
+            var nodes = textNode.Select((node, index) => (node, index))
+                .Where(x => char.IsAsciiLetter(x.node))
+                .OrderBy(x => x.index)
+                .Select((x, i) => (firstIndex: x, secondIndex: i))
+                .GroupBy(x => x.firstIndex.index - x.secondIndex)
+                .ToDictionary(x => x.Key, x => x.ToList());
+
+            var nodeName = new string(nodes[0].Select(x => x.firstIndex.node).ToArray());
+            var nextLeftNode = new string(nodes[4].Select(x => x.firstIndex.node).ToArray());
+            var nextRightNode = new string(nodes[6].Select(x => x.firstIndex.node).ToArray());
+
+            var newNode = new Node(nodeName, nextLeftNode, nextRightNode);
+            _nodeList.Add(newNode);
+        }
+
+        foreach (var node in _nodeList)
+        {
+
+            var left = node.NextNodeLeftName;
+            var right = node.NextNodeRightName;
+
+            node.NextNodeLeft = _nodeList.Find(x => x.NodeName == left);
+            node.NextNodeRight = _nodeList.Find(x => x.NodeName == right);
+        }
+
+    }
+
+    public void SolvePartOne()
+    {
+        var currentNode = _nodeList.Find(x => x.NodeName == "AAA");
+
+        int stepCounter = 0;
+        while (currentNode.NodeName != "ZZZ")
+        {
+            var currentInstuction = _instructions[stepCounter % _instructions.Length];
+
+            if(currentInstuction == 'L')
+            {
+                currentNode = currentNode.NextNodeLeft;
+            }
+            else
+            {
+                currentNode = currentNode.NextNodeRight;
+            }
+
+            stepCounter++;
+        }
+
+        Console.WriteLine(stepCounter);
+    }
+
+    public void SolvePartTwo()
+    {
+
+        var currentNodes = _nodeList.Where(x => x.NodeName[2] == 'A').ToArray();
+
+        int stepCounter = 0;
+        while (currentNodes.Any(x => x.NodeName[2] != 'Z'))
+        {
+            var currentInstuction = _instructions[stepCounter % _instructions.Length];
+
+
+            for(int i = 0; i < currentNodes.Length; i++)
+            {
+                if (currentInstuction == 'L')
+                {
+                    currentNodes[i] = currentNodes[i].NextNodeLeft;
+                }
+                else
+                {
+                    currentNodes[i] = currentNodes[i].NextNodeRight;
+                }
+            }
+
+            stepCounter++;
+        }
+
+        Console.WriteLine(stepCounter);
+    }
+}
+
+public class Node
+{
+    public Node NextNodeLeft { get; set; }
+    public Node NextNodeRight { get; set; }
+
+    public string NodeName { get; private set; }
+    public string NextNodeLeftName { get; private set; }
+    public string NextNodeRightName { get; private set; }
+
+    public Node(string nodeName, string leftNodeName, string rightNodeName)
+    {
+        NodeName = nodeName;
+        NextNodeLeftName = leftNodeName;
+        NextNodeRightName = rightNodeName;
+    }
+}
+
+
