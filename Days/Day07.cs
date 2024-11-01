df96b51701abed22170964f088fbfb2a35580dd9
Johannes Bjärehall <jojeskate@hotmail.com>
2024-11-02 00:16:11 +0100

    Solved day 7

--- Days/Day07.cs
+++ Days/Day07.cs
@@ -10,30 +10,31 @@ namespace AdventOfCode2023.Days;
 internal class Day07 : ISolution
 {
     private readonly string[] _input;
-    private readonly List<(int[] hand, int bid)> hands;
+    private readonly List<(int[] cards, int bid)> hands;
+    private readonly List<(int[] cards, int bid)> handsPartTwo;
 
-    private readonly Dictionary<string, int> groupRanks = new()
+    private readonly Dictionary<string, int> cardRanks = new()
     {
         {
-            "5", 5
+            "5", 7
         },
         {
-            "41", 4
+            "41", 6
         },
         {
-            "32", 3
+            "32", 5
         },
         {
-            "31", 3
+            "311", 4
         },
         {
-            "22", 2
+            "221", 3
         },
         {
-            "21", 1
+            "2111", 2
         },
         {
-            "1", 0
+            "11111",  1
         }
     };
 
@@ -41,11 +42,14 @@ internal class Day07 : ISolution
     {
         _input = inputService.GetInputAsLines(nameof(Day07));
         hands = _input.Select(line => ParseHand(line.Split(" ", StringSplitOptions.RemoveEmptyEntries))).ToList();
-
+        handsPartTwo = _input.Select(line => ParseHandWithJoker(line.Split(" ", StringSplitOptions.RemoveEmptyEntries))).ToList();
     }
 
     private (int[] hand, int bid) ParseHand(string[] inputLine)
         => (inputLine[0].Select(ConvertCard).ToArray(), int.Parse(inputLine[1]));
+
+    private (int[] hand, int bid) ParseHandWithJoker(string[] inputLine)
+        => (inputLine[0].Select(ConverCardWithJoker).ToArray(), int.Parse(inputLine[1]));
 
     private int ConvertCard(char card)
         => card switch
@@ -58,53 +62,145 @@ internal class Day07 : ISolution
             _ => int.Parse(card.ToString())
         };
 
-    public void SolvePartOne()
+    private int ConverCardWithJoker(char card)
+        => card switch
+        {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'T' => 10,
+            'J' => 1,
+            _ => int.Parse(card.ToString())
+        };
+
+    private int FindOptimalJokerExchange(int[] cards)
     {
-        var handRanks = hands.Select((hand, index) =>
+        int[] cardsCopy = new int[cards.Length];
+        Array.Copy(cards, cardsCopy, cards.Length);
+
+        int[] IndexOfJokers = cardsCopy
+            .Select((card, index) => (card, index))
+            .Where(x => x.card == 1)
+            .Select(x => x.index)
+            .ToArray();
+
+        int maxRank = 0;
+
+        for(int i = 1; i <= 14; i++)
         {
-            var cardGroups = hand.hand
-            .GroupBy(card => card)
-            .GroupBy(g => g.Count()).Select(x => x.Key)
-            .OrderByDescending(x => x)
-            .Aggregate("", (a, b) => a + b);
+            foreach(var index in IndexOfJokers)
+            {
+                cardsCopy[index] = i;
+            }
 
-            var rank = groupRanks[cardGroups];
+            var rank = cardsCopy
+                .GroupBy(x => x)
+                .ToList()
+                .Select(x => x.Count().ToString())
+                .OrderByDescending(x => x)
+                .Aggregate("", (a, b) => a + b);
 
-            return (rank, hand);
-        })
-        .GroupBy(hand => hand.rank)
-        .ToDictionary(hand => hand.Key, hand => hand.ToList());
+            var rankValue = cardRanks[rank];
 
-        foreach (var item in handRanks)
-        {
-            item.Value.Sort(handComparer);
+            if(rankValue > maxRank)
+                maxRank = rankValue;
         }
 
-        var result = handRanks.OrderBy(x => x.Key)
-            .SelectMany(x => x.Value)
-            .Select((value, index) => value.hand.bid * (index + 1))
-            .Sum();
-
-        Console.WriteLine(result);
+        return maxRank;
     }
 
-    private readonly Comparison<(int rank,(int[] cards, int bid) hand)> handComparer = (a, b) =>
+    public void SolvePartOne()
     {
-        for (int i = 0; i < a.hand.cards.Length; i++)
+        var rankedHands = hands.Select(hand =>
         {
-            if (a.hand.cards[i] > b.hand.cards[i])
-                return 1;
-            if (a.hand.cards[i] < b.hand.cards[i])
-                return -1;
+            var rank = hand.cards
+                .GroupBy(x => x)
+                .ToList()
+                .Select(x => x.Count().ToString())
+                .OrderByDescending(x => x)
+                .Aggregate("", (a, b) => a + b);
+
+            var rankValue = cardRanks[rank];
+
+            return (rankValue, rank, hand);
+        })
+        .GroupBy(x => x.rankValue)
+        .ToDictionary(group => group.Key, group => group.ToList());
+
+        foreach (var key in rankedHands.Keys)
+        {
+            rankedHands[key].Sort((a, b) =>
+            {
+                for (int i = 0; i < a.hand.cards.Length; i++)
+                {
+                    if (a.hand.cards[i] > b.hand.cards[i])
+                        return 1;
+                    if (a.hand.cards[i] < b.hand.cards[i])
+                        return -1;
+                }
+
+                return 0;
+            });
         }
 
-        return 0;
-    };
+        var orderedHands = rankedHands
+            .Select(x => (x.Key, x.Value))
+            .OrderBy(x => x.Key)
+            .SelectMany(x => x.Value);
+
+        int sum = orderedHands.Select((hand, index) => hand.hand.bid * (index + 1)).Sum();
+
+        Console.WriteLine($"Total winnings: {sum}");
+    }
 
     public void SolvePartTwo()
     {
-        Console.WriteLine("");
+        var rankedHands = handsPartTwo.Select(hand =>
+        {
+            var currentCards = hand.cards;
+
+            var rank = currentCards
+                .GroupBy(x => x)
+                .ToList()
+                .Select(x => x.Count().ToString())
+                .OrderByDescending(x => x)
+                .Aggregate("", (a, b) => a + b);
+
+            var rankValue = cardRanks[rank];
+
+            if (hand.cards.Contains(1))
+            {
+                rankValue = FindOptimalJokerExchange(currentCards);
+            }
+
+            return (rankValue, rank, hand);
+        })
+        .GroupBy(x => x.rankValue)
+        .ToDictionary(group => group.Key, group => group.ToList());
+
+        foreach (var key in rankedHands.Keys)
+        {
+            rankedHands[key].Sort((a, b) =>
+            {
+                for (int i = 0; i < a.hand.cards.Length; i++)
+                {
+                    if (a.hand.cards[i] > b.hand.cards[i])
+                        return 1;
+                    if (a.hand.cards[i] < b.hand.cards[i])
+                        return -1;
+                }
+
+                return 0;
+            });
+        }
+
+        var orderedHands = rankedHands
+            .Select(x => (x.Key, x.Value))
+            .OrderBy(x => x.Key)
+            .SelectMany(x => x.Value);
+
+        int sum = orderedHands.Select((hand, index) => hand.hand.bid * (index + 1)).Sum();
+
+        Console.WriteLine($"Total winnings: {sum}");
     }
-
-
 }
